abd742d014a2cc3735f0e4b4789628c45f9a5dea
moussaoui1228 <moussaouisarah78@gmail.com>
2026-04-24 14:06:59 +0100

    some details

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+export const dynamic = "force-dynamic";
 import { useState, useEffect, Suspense } from 'react';
 import { supabase } from '@/lib/supabase';
 import { useRouter, useSearchParams } from 'next/navigation';
