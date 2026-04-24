55c2735a916b514a30f42268e0dbc2f2834b19a5
moussaoui1228 <moussaouisarah78@gmail.com>
2026-04-24 14:10:54 +0100

    bd updates

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://dfumuonkcfjxpwpmynrr.supabase.co';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRmdW11b25rY2ZqeHB3cG15bnJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5NjY4OTYsImV4cCI6MjA5MjU0Mjg5Nn0.lOLjZFaZf8HDji0IsfXN5SmrpB7hniW3U-n8Sn1d6WA';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
